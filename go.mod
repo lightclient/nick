d3e1fc61cf157775686c2391cdc7ff9eb6b7e306
lightclient <lightclient@protonmail.com>
2024-07-10 10:20:21 -0600

    all: add cli

--- go.mod
+++ go.mod
@@ -2,13 +2,17 @@ module github.com/lightclient/nicks-method
 
 go 1.20
 
-require github.com/ethereum/go-ethereum v1.12.2
+require (
+	github.com/ethereum/go-ethereum v1.12.2
+	github.com/urfave/cli/v3 v3.0.0-alpha9
+	golang.org/x/crypto v0.9.0
+)
 
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/holiman/uint256 v1.2.3 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/sys v0.9.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,68 +4,163 @@ package main
 
 import (
 	"bytes"
+	"context"
 	crand "crypto/rand"
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/big"
 	"os"
 	"runtime"
-	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/urfave/cli/v3"
 	"golang.org/x/crypto/sha3"
 )
 
+var (
+	threadsFlag = &cli.IntFlag{
+		Name:  "threads",
+		Usage: "number of threads to search on",
+		Value: int64(runtime.NumCPU()),
+	}
+	scoreFlag = &cli.IntFlag{
+		Name:  "score",
+		Usage: "minimum score number to report",
+		Value: 5,
+	}
+	prefixFlag = &cli.StringFlag{
+		Name:      "prefix",
+		Usage:     "desired prefix in vanity address",
+		Validator: checkHex,
+		Value:     "0x0000",
+	}
+	suffixFlag = &cli.StringFlag{
+		Name:      "suffix",
+		Usage:     "desired suffix in vanity address",
+		Validator: checkHex,
+		Value:     "0xaaaa",
+	}
+	initcodeFlag = &cli.StringFlag{
+		Name:      "initcode",
+		Usage:     "desired initcode to deploy at vanity address",
+		Value:     "0x",
+		Validator: checkHex,
+		Required:  true,
+	}
+	gasLimitFlag = &cli.UintFlag{
+		Name:  "gaslimit",
+		Usage: "desired gas limit for deployment transaction",
+		Value: 250000,
+	}
+	gasPriceFlag = &cli.UintFlag{
+		Name:  "gasprice",
+		Usage: "desired gas price (gwei) for deployment transaction",
+		Value: 1000,
+	}
+
+	app = &cli.Command{
+		Commands: []*cli.Command{
+			{
+				Name:  "search",
+				Usage: "Search for a vanity address to deploy a contract using nicks method.",
+				Flags: []cli.Flag{threadsFlag, scoreFlag, prefixFlag, suffixFlag,
+					initcodeFlag, gasLimitFlag, gasPriceFlag},
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					f := task{
+						prefix:    common.FromHex(cmd.String(prefixFlag.Name)),
+						suffix:    common.FromHex(cmd.String(suffixFlag.Name)),
+						initcode:  common.FromHex(cmd.String(initcodeFlag.Name)),
+						gasLimit:  cmd.Uint(gasLimitFlag.Name),
+						gasPrice:  cmd.Uint(gasPriceFlag.Name),
+						threads:   cmd.Int(threadsFlag.Name),
+						score:     int(cmd.Int(scoreFlag.Name)),
+						highscore: &atomic.Uint64{},
+						count:     &atomic.Uint64{},
+						quit:      make(chan struct{}),
+					}
+					return f.run()
+				},
+			},
+			{
+				Name:      "print",
+				Usage:     "Print reads a json tx object from file and prints the deployment info.",
+				ArgsUsage: "[filename]",
+				Action:    print,
+			},
+		},
+	}
+)
+
 func main() {
+	if err := app.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// task represents a search for a vanity deployment address.
+type task struct {
+	prefix   []byte
+	suffix   []byte
+	initcode []byte
+
+	gasLimit uint64
+	gasPrice uint64
+
+	threads int64
+	score   int
+
+	highscore *atomic.Uint64
+	count     *atomic.Uint64
+	quit      chan struct{}
+}
+
+func (f *task) run() error {
 	go func() {
 		logTime := time.Now()
 		for {
 			if time.Since(logTime) > time.Second*30 {
-				fmt.Printf("Did %d attempts in %v, best score is %d\n", count.Load(), time.Since(logTime), highscore.Load())
+				fmt.Printf("Did %d attempts in %v, best score is %d\n", f.count.Load(), time.Since(logTime), f.highscore.Load())
 				logTime = time.Now()
-				count.Store(0)
+				f.count.Store(0)
 			}
 		}
 	}()
-	threads := runtime.NumCPU()
-	if len(os.Args) == 2 {
-		val, _ := strconv.ParseInt(os.Args[1], 10, 64)
-		threads = int(val)
+
+	// Spin up workers.
+	for i := 0; i < int(f.threads); i++ {
+		go f.brute()
 	}
-	fmt.Println("thread count:", threads)
-	for i := 0; i < threads; i++ {
-		go brute(common.FromHex("0000000000"), common.FromHex("0xbeac02"))
-	}
-	brute(common.FromHex("0000000000"), common.FromHex("0xbeac02"))
+
+	<-f.quit
+	return nil
 }
 
-var highscore atomic.Int64
-var count atomic.Uint64
-
-func brute(start []byte, end []byte) {
+// brute runs the brute force seacher on a single thread.
+func (t *task) brute() {
 	var (
 		inner = types.LegacyTx{
 			Nonce:    0,
-			GasPrice: newGwei(1000),
-			Gas:      250000,
+			GasPrice: newGwei(t.gasPrice),
+			Gas:      t.gasLimit,
 			To:       nil,
 			Value:    big.NewInt(0),
-			Data:     common.FromHex("0x60618060095f395ff33373fffffffffffffffffffffffffffffffffffffffe14604d57602036146024575f5ffd5b5f35801560495762001fff810690815414603c575f5ffd5b62001fff01545f5260205ff35b5f5ffd5b62001fff42064281555f359062001fff015500"),
+			Data:     t.initcode,
 			V:        big.NewInt(27),
 			R:        big.NewInt(0x539),
 			S:        big.NewInt(0x1337),
 		}
-		hash      = sighash(types.NewTx(&inner))
-		bigbig, _ = new(big.Int).SetString("0x1337000000000000000000", 0)
-		u64       = make([]byte, 8)
+		hash = sighash(types.NewTx(&inner))
+		u64  = make([]byte, 8)
 	)
 	for {
 		sender, err := recoverPlain(hash, inner.R, inner.S, inner.V)
@@ -74,26 +169,53 @@ func brute(start []byte, end []byte) {
 		}
 		addr := crypto.CreateAddress(sender, 0)
 
-		score := compare(end, addr[len(addr)-len(end):])
-		if score == len(end)*2 {
-			score += compare(start, addr[:])
-			if int64(score) > highscore.Load() {
-				highscore.Store(int64(score))
+		if bytes.Equal(addr[len(addr)-len(t.suffix):], t.suffix) {
+			score := compare(t.prefix, addr[:]) + len(t.suffix)*2
+			if uint64(score) > t.highscore.Load() {
+				t.highscore.Store(uint64(score))
 			}
-			if score >= 8 {
+			if score >= t.score {
 				tx := types.NewTx(&inner)
 				txjson, _ := json.MarshalIndent(tx, "", "  ")
 				fmt.Printf("New highscore: %d\nSender: %v\nAddress: %v\nTx:\n%v\n", score, sender, addr, string(txjson))
 			}
 		}
-
 		crand.Read(u64)
 		inner.S = new(big.Int).SetUint64(binary.BigEndian.Uint64(u64))
-		inner.S.Add(inner.S, bigbig)
-		count.Add(1)
+		t.count.Add(1)
 	}
 }
 
+// print recomputes the deployer and deployment address from a tx json and
+// prints the result.
+func print(ctx context.Context, cmd *cli.Command) error {
+	b, err := os.ReadFile(cmd.Args().First())
+	if err != nil {
+		return fmt.Errorf("unable to read file: %w", err)
+	}
+	var tx types.Transaction
+	if err := tx.UnmarshalJSON(b); err != nil {
+		return fmt.Errorf("unable to parse tx: %w", err)
+	}
+	signer := types.LatestSignerForChainID(common.Big1)
+	sender, err := signer.Sender(&tx)
+	if err != nil {
+		return fmt.Errorf("failed to recover tx sender: %w", err)
+	}
+	addr := crypto.CreateAddress(sender, 0)
+	fmt.Printf("Sender: %v\nAddress: %v", sender, addr)
+	return nil
+}
+
+// checkHex verifies the string is a proper hex value.
+func checkHex(s string) error {
+	if _, err := hexutil.Decode(s); err != nil {
+		return fmt.Errorf("flag value must be hex: got=%v err=%v", s, err)
+	}
+	return nil
+}
+
+// compare returns the number of matching nibbles across a and b.
 func compare(a, b []byte) int {
 	for i, x := range a {
 		y := b[i]
@@ -107,8 +229,20 @@ func compare(a, b []byte) int {
 	return 2 * len(a)
 }
 
-func newGwei(n int64) *big.Int {
-	return new(big.Int).Mul(big.NewInt(n), big.NewInt(params.GWei))
+func newGwei(n uint64) *big.Int {
+	return new(big.Int).Mul(big.NewInt(int64(n)), big.NewInt(params.GWei))
+}
+
+// sighash computes the hash which will be signed over.
+func sighash(tx *types.Transaction) common.Hash {
+	return rlpHash([]interface{}{
+		tx.Nonce(),
+		tx.GasPrice(),
+		tx.Gas(),
+		tx.To(),
+		tx.Value(),
+		tx.Data(),
+	})
 }
 
 func recoverPlain(sighash common.Hash, R, S, Vb *big.Int) (common.Address, error) {
@@ -130,17 +264,6 @@ func recoverPlain(sighash common.Hash, R, S, Vb *big.Int) (common.Address, error
 	var addr common.Address
 	copy(addr[:], crypto.Keccak256(pub[1:])[12:])
 	return addr, nil
-}
-
-func sighash(tx *types.Transaction) common.Hash {
-	return rlpHash([]interface{}{
-		tx.Nonce(),
-		tx.GasPrice(),
-		tx.Gas(),
-		tx.To(),
-		tx.Value(),
-		tx.Data(),
-	})
 }
 
 // hasherPool holds LegacyKeccak256 hashers for rlpHash.
